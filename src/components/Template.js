ddea309d83913ee3bcaf8bee857778e5630ac6ac
Gangbaekho <roorooro9933@gmail.com>
2020-09-14 17:14:27 +0900

    react init

---
+++ src/components/Template.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Template extends React.Component{
+
+    render(){
+        return (
+            <div>
+                <h1>This is my first Template!</h1>
+            </div>
+        )
+    }
+}
+
+export default Template
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Template from './components/Template'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Template/>
   </React.StrictMode>,
   document.getElementById('root')
 );
